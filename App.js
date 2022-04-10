800bb5dd5a23414b25ce73fbe150125db2f1bd3c
guilhermeportella <guilhermeportella2@gmail.com>
2022-04-09 21:56:55 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import { StatusBar } from 'expo-status-bar';
+
+import Home from './src/views/Home';
+
+export default function App() {
+  return <Home/>
+}
+
---
+++ src/views/Home/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF7',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
+
+export default styles;
