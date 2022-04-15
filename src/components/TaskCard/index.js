b7533d37a30c9f9d1b7c1e8e94bb987362ebd366
guilhermeportella <guilhermeportella2@gmail.com>
2022-04-15 00:20:39 -0300

    Incluindo date-fns

--- src/components/TaskCard/index.js
+++ src/components/TaskCard/index.js
@@ -5,23 +5,23 @@ import styles from './styles'
 
 import iconDefault from '../../assets/default.png'
 
-export default function TaskCard({done}){
+export default function TaskCard({done, title, when}){
     return (
         <TouchableOpacity style={[styles.card, done && styles.done]}>
             <View style={styles.cardLeft}>
                 <Image source={iconDefault} style={styles.typeActive}/>
                 <Text style={styles.cardTitle}>
-                    Fazer Relatorio
+                    {title}
                 </Text>
             </View>
 
             <View style={styles.cardRight}>
                 <Text style={styles.cardDate}>
-                    17/12/2020
+                    {when}
                 </Text>
 
                 <Text style={styles.cardTime}>
-                    10:00
+                    {when}
                 </Text>
                 
             </View>
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -78,7 +78,7 @@ export default function Home() {
                     <ActivityIndicator color='#EE6B26' size={50}/>
                     : 
                     tasks.map(t => (
-                        <TaskCard done={false} />
+                        <TaskCard done={false} title={t.title} when={t.when}/>
                     ))
                 }
             </ScrollView>
